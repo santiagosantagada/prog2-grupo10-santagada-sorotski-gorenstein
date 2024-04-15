904cfd430999021b2a9dc2f88b5c64df2a4b993c
Santiago Santagada <santi@Santiagos-MacBook-Air-3.local>
2024-04-15 10:44:26 -0300

    agregue productos en el modulo de datos

--- db/index.js
+++ db/index.js
@@ -5,7 +5,95 @@ const datos = {
         fechaNacimiento: "20-10-2000" ,
         dni: "46364341" ,
         fotoPerfil: "",//poner link de una foto 
-    }
+    } ,
+    productos: [
+        {
+        imagen: "https://www.lg.com/ar/images/televisores/md06198056/gallery/des_1_n.jpg" ,
+        nombre_producto: "Television" , 
+        descripcion: "Televisión 60 pulgadas" ,
+        comentarios: [{
+            nombre_usuario: "francisco08" ,
+            texto: "Muy caro!" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "elkusa_104" ,
+            texto: "Tiene netflix?" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "martin_hernandez" ,
+            texto: "Parece de muy buena calidad" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" }]
+        },
+        {
+        imagen: "https://images.samsung.com/is/image/samsung/p6pim/ar/lc24rg50fzlczb/gallery/ar-c24rg5-433672-lc24rg50fzlczb-533475182?$650_519_PNG$" ,
+        nombre_producto: "Monitor" , 
+        descripcion: "Monitor nuevo 144hz" ,
+        comentarios: [{
+            nombre_usuario: "cusita91" ,
+            texto: "Podemos negociar el precio?" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "sorots_" ,
+            texto: "Muy buen monitor" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "santa44_" ,
+            texto: "¡Increible!" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" }]
+        },
+    {
+        imagen: "https://arsonyb2c.vtexassets.com/arquivos/ids/362074/PlayStation-5-DualShock.jpg?v=638162215101070000" ,
+        nombre_producto: "PlayStation 5" , 
+        descripcion: "Nueva PlayStation edición limitada" ,
+        comentarios: [{
+            nombre_usuario: "joakito_kusin" ,
+            texto: "Es la nueva?" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "marcosgubi" ,
+            texto: "Muy buen producto!!" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "shawn_mendez" ,
+            texto: "Trae juegos??" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" }]
+        },
+    {
+        imagen: "https://www.compu-santafe.com.ar/productos/get-imagen/39013" ,
+        nombre_producto: "Patineta eléctrica" , 
+        descripcion: "Patineta eléctrica usada con dibujos únicos" ,
+        comentarios: [{
+            nombre_usuario: "joaco_kusa" ,
+            texto: "Muy buenaaa" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "mia_goren" ,
+            texto: "Que buenos dibujos" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "argos2009" ,
+            texto: "Precio?" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" }]
+        },
+    {
+        imagen: "https://ecomotors.com.ar/wp-content/uploads/2023/02/ecomotors-bicicleta-electrica-z3-1.jpg" ,
+        nombre_producto: "Bicicleta eléctrica" , 
+        descripcion: "Bicicleta eléctrica 0km" ,
+        comentarios: [{
+            nombre_usuario: "agustin2004" ,
+            texto: "Hay otro color?" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "tomy_fer" ,
+            texto: "Increiblee" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" } ,
+            {
+            nombre_usuario: "francisco_07" ,
+            texto: "Muy buena bicicleta" ,
+            foto_perfil: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" }]
+        },
+
+]
 }
 
 module.exports = datos;
