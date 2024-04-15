21766e23ce072487b2cdb2749b8a214fa3ce307c
miagorenstein <mgorenstein@udesa.edu.ar>
2024-04-14 23:44:39 -0300

    agregue usuario en datos del index db

--- db/index.js
+++ db/index.js
@@ -1,6 +1,10 @@
 const datos = {
     usuario: {
-
+        email: "luis@gmail.com" ,
+        contraseña: "amodigital" ,
+        fecha-nacimiento: "20-10-2000" ,
+        dni: "46364341" ,
+        foto-perfil: ,//poner link de una foto 
     }, 
     productos: [
 
