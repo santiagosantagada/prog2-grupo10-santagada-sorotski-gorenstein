495aa6d266ad7e56c82bd2ed268e6c05db9fadfb
Santiago Santagada <santi@Santiagos-MacBook-Air-3.local>
2024-04-15 13:53:26 -0300

    Cambios en db y sql

--- db/data.sql
+++ db/data.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA data_1;
+CREATE SCHEMA data_3;
 
-USE data_1;
+USE data_3;
 
 CREATE TABLE usuarios (
 /* 	nombreColumna 		tipoDato 		Restricciones */
@@ -47,7 +47,7 @@ INSERT INTO productos (id,idUsuario,nombreImagen,nombreProducto,descripcion,crea
 VALUES (DEFAULT , DEFAULT,"Gran Telefono " , "Telefono" , "Iphone nuevo, sin uso" ,DEFAULT, DEFAULT , DEFAULT) ;
 INSERT INTO productos (id,idUsuario,nombreImagen,nombreProducto,descripcion,createdAt,updatedAt,deletedAt) 
 VALUES (DEFAULT , DEFAULT,"Increibles auriculares" , "Auriculares" , "Escucha perfecta asegurada. inalambricos" ,DEFAULT, DEFAULT , DEFAULT) ;
-INSERT INTO productos (id,idUsuarionombreImagen,nombreProducto,descripcion,createdAt,updatedAt,deletedAt) 
+INSERT INTO productos (id,idUsuario,nombreImagen,nombreProducto,descripcion,createdAt,updatedAt,deletedAt) 
 VALUES (DEFAULT ,DEFAULT, "Carga perfecta" , "Cargador" , "El mejor cargador que puedas encontrar" ,DEFAULT, DEFAULT , DEFAULT) ;
 INSERT INTO productos (id,idUsuario,nombreImagen,nombreProducto,descripcion,createdAt,updatedAt,deletedAt) 
 VALUES (DEFAULT ,DEFAULT, "Ipod nuevo" , "Ipod" , "Recien llegado a Argentina, muy bueno" ,DEFAULT, DEFAULT , DEFAULT) ;
@@ -67,54 +67,53 @@ CREATE TABLE comentarios (
 /* 	nombreColumna 		tipoDato 		Restricciones */
     id 					  	INT 		UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     idProducto				INT 		UNSIGNED,
+    idAutor					INT			unsigned,
     textoComentario 	VARCHAR(250) 	NOT NULL,
     FOREIGN KEY (idProducto) REFERENCES productos(id),
+    foreign key (idAutor) references usuarios(id),
     createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
 	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	deletedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO comentarios (id, idProducto, textoComentario, createdAt, updatedAt, deletedAt) VALUES 
-(DEFAULT, DEFAULT, "¡Impresionante! Funciona a la perfección.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Me encanta este producto, ¡totalmente recomendado!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Excelente estado, como nuevo. ¡Estoy muy satisfecho!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Increíble oferta por un producto de alta calidad.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Muy contento con mi compra, todo en perfectas condiciones.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Producto impecable, exactamente como se describe.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Fantástico! Funciona sin ningún problema.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡No podría estar más feliz con mi compra!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Estoy asombrado por la calidad de este producto.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Muy satisfecho con esta compra, vale cada centavo!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Increíblemente bueno! Definitivamente vale la pena.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "No puedo creer lo bien que funciona este producto.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Exactamente lo que estaba buscando, ¡perfecto!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Impresionado por la calidad y el rendimiento.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Mejor de lo que esperaba! Totalmente recomendado.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Producto de primera calidad a un precio excelente!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Funciona de maravilla, estoy muy impresionado.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Exactamente lo que necesitaba! Funciona a la perfección.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡No puedo creer lo bien que este producto funciona!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Estoy muy contento con esta compra, sin duda volveré!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "La calidad de este producto es excepcional.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Funciona como un encanto! Muy feliz con mi compra.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "Increíblemente satisfecho con este producto.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Producto excelente a un precio inmejorable!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡No puedo dejar de recomendar este producto!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Absolutamente asombroso! Cumple con todas mis expectativas.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Mejor de lo que esperaba! Una verdadera ganga.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Estoy sorprendido por lo bien que funciona este producto!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Muy impresionado! Definitivamente vale la pena.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Exactamente lo que necesitaba! Funciona perfectamente.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Increíble oferta por un producto de alta gama!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Realmente superó mis expectativas! Estoy encantado.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡No puedo creer lo bueno que es este producto!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Totalmente satisfecho con esta compra, gracias!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Producto de calidad superior a un precio excelente!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡No puedo recomendar este producto lo suficiente!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Fantástico! Funciona de manera impecable.", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Mejor compra que he hecho en mucho tiempo!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Estoy muy impresionado por la calidad de este producto!", DEFAULT, DEFAULT, DEFAULT),
-(DEFAULT, DEFAULT, "¡Increíblemente feliz con mi compra, gracias por todo!", DEFAULT, DEFAULT, DEFAULT);
-
-
-module.exports= productos
+INSERT INTO comentarios (idProducto, idAutor, textoComentario) VALUES 
+(1, 1, "¡Impresionante! Funciona a la perfección."),
+(2, 1, "Me encanta este producto, ¡totalmente recomendado!"),
+(3, 1, "Excelente estado, como nuevo. ¡Estoy muy satisfecho!"),
+(4, 1, "Increíble oferta por un producto de alta calidad."),
+(5, 2, "Muy contento con mi compra, todo en perfectas condiciones."),
+(6, 2, "Producto impecable, exactamente como se describe."),
+(7, 2, "¡Fantástico! Funciona sin ningún problema."),
+(8, 2, "¡No podría estar más feliz con mi compra!"),
+(9, 3, "Estoy asombrado por la calidad de este producto."),
+(10, 3, "¡Muy satisfecho con esta compra, vale cada centavo!"),
+(1, 3, "¡Increíblemente bueno! Definitivamente vale la pena."),
+(2, 3, "No puedo creer lo bien que funciona este producto."),
+(3, 4, "Exactamente lo que estaba buscando, ¡perfecto!"),
+(4, 4, "Impresionado por la calidad y el rendimiento."),
+(5, 4, "¡Mejor de lo que esperaba! Totalmente recomendado."),
+(6, 4, "¡Producto de primera calidad a un precio excelente!"),
+(7, 5, "Funciona de maravilla, estoy muy impresionado."),
+(8, 5, "¡Exactamente lo que necesitaba! Funciona a la perfección."),
+(9, 5, "¡No puedo creer lo bien que este producto funciona!"),
+(10, 5, "¡Estoy muy contento con esta compra, sin duda volveré!"),
+(1, 1, "La calidad de este producto es excepcional."),
+(2, 1, "¡Funciona como un encanto! Muy feliz con mi compra."),
+(3, 1, "Increíblemente satisfecho con este producto."),
+(4, 1, "¡Producto excelente a un precio inmejorable!"),
+(5, 2, "¡No puedo dejar de recomendar este producto!"),
+(6, 2, "¡Absolutamente asombroso! Cumple con todas mis expectativas."),
+(7, 2, "¡Mejor de lo que esperaba! Una verdadera ganga."),
+(8, 2, "¡Estoy sorprendido por lo bien que funciona este producto!"),
+(9, 3, "¡Muy impresionado! Definitivamente vale la pena."),
+(10, 3, "¡Exactamente lo que necesitaba! Funciona perfectamente."),
+(1, 3, "¡Increíble oferta por un producto de alta gama!"),
+(2, 3, "¡Realmente superó mis expectativas! Estoy encantado."),
+(3, 4, "¡No puedo creer lo bueno que es este producto!"),
+(4, 4, "¡Totalmente satisfecho con esta compra, gracias!"),
+(5, 4, "¡Producto de calidad superior a un precio excelente!"),
+(6, 4, "¡No puedo recomendar este producto lo suficiente!"),
+(7, 5, "¡Fantástico! Funciona de manera impecable."),
+(8, 5, "¡Mejor compra que he hecho en mucho tiempo!"),
+(9, 5, "¡Estoy muy impresionado por la calidad de este producto!"),
+(10, 5, "¡Increíblemente feliz con mi compra, gracias por todo!");
