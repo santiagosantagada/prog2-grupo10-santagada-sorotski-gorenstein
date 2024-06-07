aa88cc6c212e85381c98ce4dc1d4a99197644aee
AgosSorotski <asorotski@udesa.edu.ar>
2024-06-07 13:21:35 -0300

    modelo de productos y cambio en productosController

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -3,8 +3,22 @@ const {Association} = require("sequelize")
 
 const productosController= {
     product: function(req, res){
-        return res.render("product", {
-            datos: datos
+        datos.Producto.findAll()
+
+        //return res.render("product", {datos: datos.productos})
+        let filtrado= {
+            include: [
+                {association: "product"}
+
+            ]
+        }
+        datos.Producto.findAll(filtrado)
+        .then(function (results) {
+            return res.send(results)
+            return res.render("product", {datos: results})
+        })
+        .catch(function(error){
+            return console.log(error)
         })
     },
     searchresults: function(req, res){
--- database/models/Productos.js
+++ database/models/Productos.js
@@ -1,1 +1,49 @@
-p
+module.exports = function(sequelize, dataTypes) {
+
+    /* alias */
+    let alias = "Producto";
+
+    /* configuracion de las columnas */
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        nombreImagen: {
+            type: dataTypes.STRING
+        },
+        nombreProducto: {
+            type: dataTypes.STRING
+        },
+        descripcion:{
+            type: dataTypes.STRING
+        }
+    }
+   
+		
+
+    /* config de la tabla */
+    let config = {
+        tableName: "productos",
+        timestamps: false,
+        underscored: false
+    }
+
+    /* definir el modelo */
+
+    let Producto = sequelize.define(alias, cols, config);
+
+   Producto.associate = function(models) {
+
+        //todas las relaciones
+        Producto.belongsTo(models.Usuario, {
+            as: "user",
+            foreignKey: "idUsuario"
+        })
+    }
+    
+  
+    
+    return Producto;
+}
